c9d692b30ffe37c653b683c7a7140af7a7685d38
Tyler Gan <zhaolianggan@gmail.com>
2021-04-16 14:37:43 +0800

    First Commit

---
+++ AspNetCoreWIthReactApp/AspNetCoreWIthReactApp/ClientApp/src/components/MyDemo.js
@@ -0,0 +1,15 @@
+﻿import React, { Component } from 'react'
+
+export class MyDemo extends Component {
+    constructor(props) {
+        super(props)
+    };
+    render() {
+        return (
+            <div>
+                <h1>Hello World</h1>
+                <p></p>
+            </div>)
+    };
+};
+};
